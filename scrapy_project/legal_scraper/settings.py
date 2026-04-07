626c57b57b2379ade5bcb574aafe79225bc47c2a
Charbel Saliba <charbel_cg@outlook.com>
2026-04-07 11:13:46 +0300

    clean up scripts

--- scrapy_project/legal_scraper/settings.py
+++ scrapy_project/legal_scraper/settings.py
@@ -1,5 +1,10 @@
 import os
+from pathlib import Path
 from typing import Any
+
+from dotenv import load_dotenv
+
+load_dotenv(Path(__file__).parent.parent.parent / ".env")
 
 BOT_NAME = "legal_scraper"
 
